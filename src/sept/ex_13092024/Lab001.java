b7f4ef69146cfc9941b26c092ae0044836f3ed9d
Preeti <preetidhyani21@gmail.com>
2024-09-16 05:28:42 +0530

    Add the files

--- src/sept/ex_13092024/Lab001.java
+++ src/sept/ex_13092024/Lab001.java
@@ -1,4 +1,19 @@
 package sept.ex_13092024;
 
 public class Lab001 {
+    public static void main(String[] preeti)
+    {
+//        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+    ;
 }
