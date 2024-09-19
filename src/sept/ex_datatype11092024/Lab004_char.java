a7ec0ecc63ae03fa20f96568c4d1722edd3e7243
Preeti <preetidhyani21@gmail.com>
2024-09-20 03:13:48 +0530

    Add the files

---
+++ src/sept/ex_datatype11092024/Lab004_char.java
@@ -0,0 +1,15 @@
+package sept.ex_datatype11092024;
+
+public class Lab004_char {
+    public static void main(String[] args) {
+        char c = 'A';
+        System.out.println(c);
+        char c1 = '$';
+        System.out.println(c1);
+        char c2 = '\n';
+        System.out.println(c2);
+        char c3 = '\u1F6A'; //UNICODE CHARACTER
+        System.out.println(c3);
+
+    }
+}
