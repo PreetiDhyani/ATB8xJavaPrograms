11075de42abdac6267162b9e70e09e6c965d8fdb
Preeti <preetidhyani21@gmail.com>
2024-09-20 03:07:06 +0530

    Add the files

--- src/sept/ex_datatype11092024/Lab003_intergralsize.java
+++ src/sept/ex_datatype11092024/Lab003_intergralsize.java
@@ -7,17 +7,17 @@ public class Lab003_intergralsize {
         short s = 128; //16 bite, 2 byte
         int i = 1000; //32 bit, 4 byte
         long phone_num1 = 987654321; // 64 bit, 8byte
-        char grade = 'A' // 'A', a,b,c,d,c--> ASCII value- int assigned.
+        char grade = 'A'; // 'A', a,b,c,d,c--> ASCII value- int assigned.
         //16 bits, 2 byte
         float f = 3.14f;
         float f2 = 3.14F; //32 Bites, 8bytes
 
         //non primitive data types (User defined)
-        String name = "Pramod"; //String is bunch of char(collection of chars)
+        String name = "Pramod"; //String is a bunch of char(collection of chars)
         //Array, class-user Data types.
         //Store the distance between earth and moon
-        Long distance_between_earth_and_moon = 38440000L; in M
-        double distance_between_earth_and_moon2 = 38440000000.865; in M
+        Long distance_between_earth_and_moon = 38440000L; //in Meter
+        double distance_between_earth_and_moon2 = 38440000000.865; //in Meters
 
                 //max age = 122;
 
