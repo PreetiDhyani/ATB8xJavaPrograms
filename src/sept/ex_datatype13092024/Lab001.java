9b0446010d2cb0275b0ee00e9a671a730d41f5cb
Preeti <preetidhyani21@gmail.com>
2024-09-20 04:10:02 +0530

    Add the files

---
+++ src/sept/ex_datatype13092024/Lab001.java
@@ -0,0 +1,17 @@
+package sept.ex_datatype13092024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        // System.out.println() Vs System.out.print()
+        System.out.println("Hello World");
+        System.out.println("Parmod");
+        // ln pushes another print statement in next line,
+        // System.out.println() >
+        //Hello World
+        //Parmod
+        // System.out.print() > without ln
+        //Hello World Parmod
+
+
+    }
+}
