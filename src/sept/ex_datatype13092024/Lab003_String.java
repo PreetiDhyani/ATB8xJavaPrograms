3f016ec4f29c904b2d925c3a5c93e374ef094b4f
Preeti <preetidhyani21@gmail.com>
2024-09-20 04:31:38 +0530

    Add the files

---
+++ src/sept/ex_datatype13092024/Lab003_String.java
@@ -0,0 +1,9 @@
+package sept.ex_datatype13092024;
+
+public class Lab003_String {
+    public static void main(String[] args) {
+        String name = "Preeti";
+                String last_name = "Preeti";
+        System.out.printf("Your name is %s and Last name is %s",name, last_name);
+    }
+}
