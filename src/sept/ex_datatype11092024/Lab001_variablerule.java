3050e3499f4af3668aba2691072c2aec31946cd6
Preeti <preetidhyani21@gmail.com>
2024-09-20 02:54:18 +0530

    Add the files

---
+++ src/sept/ex_datatype11092024/Lab001_variablerule.java
@@ -0,0 +1,13 @@
+package sept.ex_datatype11092024;
+
+public class Lab001_variablerule {
+    public static void main(String[] args) {
+        //write code.
+        //data type variable_name = variable_value
+        boolean $ = true;
+        boolean _Preeti = false;
+        boolean _123 = false;
+        System.out.println(_123);
+
+    }
+}
---
+++ src/sept/ex_datatype11092024/Lab002_datatypes.java
@@ -0,0 +1,17 @@
+package sept.ex_datatype11092024;
+
+public class Lab002_datatypes {
+    public static void main(String[] args) {
+        // Data types variable_name(identifiers) = variable_name(literal)
+        //Data types
+        //Boolean & Numerical type
+        // Integral- byte, short,int, long char
+        //size- how much they can store
+        byte b = 123;
+        short s = 128;
+        System.out.println(b);
+        System.out.println(s);
+    }
+
+
+}
