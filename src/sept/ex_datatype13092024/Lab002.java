f4e57c24637440ee3d7cce86d5fe39c79f99b222
Preeti <preetidhyani21@gmail.com>
2024-09-20 04:24:54 +0530

    Add the files

---
+++ src/sept/ex_datatype13092024/Lab002.java
@@ -0,0 +1,25 @@
+package sept.ex_datatype13092024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+         // Problem statement - Print the table of 2
+        //formatting
+        int num = 2;
+        //%d-any integer, byte, short, long
+        //%s- String
+        //%c-char
+        //f- float, double
+        System.out.printf("%d * %d = %d",num,1,num*1);
+        System.out.printf("%d * %d = %d",num,2,num*2);
+        System.out.printf("%d * %d = %d",num,3,num*3);
+        System.out.printf("%d * %d = %d",num,4,num*4);
+        System.out.printf("%d * %d = %d",num,5,num*5);
+        System.out.printf("%d * %d = %d",num,6,num*6);
+        System.out.printf("%d * %d = %d",num,7,num*7);
+        System.out.printf("%d * %d = %d",num,8,num*8);
+        System.out.printf("%d * %d = %d",num,9,num*9);
+        System.out.printf("%d * %d = %d",num,10,num*10);
+
+
+         }
+}
