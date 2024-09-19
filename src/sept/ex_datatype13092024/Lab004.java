228a8d96342cea6bca0832a9428b15648ea43fb3
Preeti <preetidhyani21@gmail.com>
2024-09-20 05:01:30 +0530

    Add the files

---
+++ src/sept/ex_datatype13092024/Lab004.java
@@ -0,0 +1,23 @@
+package sept.ex_datatype13092024;
+
+public class Lab004  {
+    // Literals
+    //First type literal: Integer literal - Decimal system
+    int age = 78;
+
+    //Binary literal
+    int binary_num = ob1010;
+
+    // Octal Base
+    int octal = 0101;// 0101 = (0x a3) + (0 x a2) + (0xa0) = 65
+
+    // Hexadecimal Literals
+
+    int hex = 0xface; //base 16, color combination are in octal
+
+
+
+    //
+
+
+}
