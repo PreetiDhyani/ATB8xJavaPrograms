c9c99cdeb7a4b70c4e2ee8319f31ea18c2858ccc
Preeti <preetidhyani21@gmail.com>
2024-09-20 04:26:30 +0530

    Add the files

--- src/sept/ex_datatype13092024/Lab002.java
+++ src/sept/ex_datatype13092024/Lab002.java
@@ -9,16 +9,16 @@ public class Lab002 {
         //%s- String
         //%c-char
         //f- float, double
-        System.out.printf("%d * %d = %d",num,1,num*1);
-        System.out.printf("%d * %d = %d",num,2,num*2);
-        System.out.printf("%d * %d = %d",num,3,num*3);
-        System.out.printf("%d * %d = %d",num,4,num*4);
-        System.out.printf("%d * %d = %d",num,5,num*5);
-        System.out.printf("%d * %d = %d",num,6,num*6);
-        System.out.printf("%d * %d = %d",num,7,num*7);
-        System.out.printf("%d * %d = %d",num,8,num*8);
-        System.out.printf("%d * %d = %d",num,9,num*9);
-        System.out.printf("%d * %d = %d",num,10,num*10);
+        System.out.printf("%d * %d = %d \n",num,1,num*1);
+        System.out.printf("%d * %d = %d \n",num,2,num*2);
+        System.out.printf("%d * %d = %d \n",num,3,num*3);
+        System.out.printf("%d * %d = %d \n",num,4,num*4);
+        System.out.printf("%d * %d = %d \n",num,5,num*5);
+        System.out.printf("%d * %d = %d \n",num,6,num*6);
+        System.out.printf("%d * %d = %d \n",num,7,num*7);
+        System.out.printf("%d * %d = %d \n",num,8,num*8);
+        System.out.printf("%d * %d = %d \n",num,9,num*9);
+        System.out.printf("%d * %d = %d \n",num,10,num*10);
 
 
          }
