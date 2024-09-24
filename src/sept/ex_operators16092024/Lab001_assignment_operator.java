1371227b2fe9d52c400993ea854407675d30e2ce
Preeti <preetidhyani21@gmail.com>
2024-09-24 23:12:25 +0530

    All the files

---
+++ src/sept/ex_operators16092024/Lab001_assignment_operator.java
@@ -0,0 +1,9 @@
+package sept.ex_operators16092024;
+
+public class Lab001_assignment_operator {
+    public static void main(String[] args) {
+        int age = 45;
+        // Assignment operator
+        // It assigns right value to left variable or operands
+    }
+}
---
+++ src/sept/ex_operators16092024/Lab002_arithmetic_operator.java
@@ -0,0 +1,16 @@
+package sept.ex_operators16092024;
+
+public class Lab002_arithmetic_operator {
+    public static void main(String[] args) {
+        //Arithmetic operator
+        // =,-,/,*,%
+
+        int a=10;
+        int b=2;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
