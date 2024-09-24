1ab4b82c786a4c90dbd6b281518b6ed5453c013f
Preeti <preetidhyani21@gmail.com>
2024-09-25 04:39:19 +0530

    All the files

---
+++ src/sept/ex_operators16092024/Lab006_relational_operator.java
@@ -0,0 +1,23 @@
+package sept.ex_operators16092024;
+
+public class Lab006_relational_operator {
+    public static void main(String[] args) {
+        //Relational operators = boolean
+        // ><,>=, <=,!=, ! >> they always return true or false
+        int a = 10;
+        int b = 30;
+        //boolean c = a > b;
+        //System.out.println(c);
+        //boolean c = a < b;
+        //System.out.println(c);
+        //boolean c = a >= b; // a>b or a=b both conditions applied
+        //System.out.println(c);
+        //boolean c = a <= b; // a<b or a=b both conditions applied
+        //System.out.println(c);
+        //boolean c = a != b; // a!b or a=b both conditions applied
+        //System.out.println(c);
+
+
+
+    }
+}
