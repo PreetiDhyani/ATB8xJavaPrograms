8afe9d28d0c93063eced73086940ac3d184ad191
Preeti <preetidhyani21@gmail.com>
2024-09-24 23:31:45 +0530

    All the files

---
+++ src/sept/ex_operators16092024/Lab004_concatenation.java
@@ -0,0 +1,13 @@
+package sept.ex_operators16092024;
+
+public class Lab004_concatenation {
+    public static void main(String[] args) {
+        String first_name = "Preeti";
+        String last_name = "Dhyani";
+        System.out.println("Preeti" + "Dhyani");
+        // Concatenation operator>> +
+        // = >> in case of int +int = addition _math
+        // In case of string it combines
+
+    }
+}
