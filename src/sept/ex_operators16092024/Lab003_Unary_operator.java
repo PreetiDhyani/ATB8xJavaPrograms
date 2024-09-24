92943c36c86985fd01b3b059515437bb74f7a053
Preeti <preetidhyani21@gmail.com>
2024-09-24 23:26:57 +0530

    All the files

---
+++ src/sept/ex_operators16092024/Lab003_Unary_operator.java
@@ -0,0 +1,14 @@
+package sept.ex_operators16092024;
+
+public class Lab003_Unary_operator {
+    public static void main(String[] args) {
+        //unary operator
+        int a = +65; // + >> Unary operator
+        int b = -1; // - >> Unary operator
+        System.out.println(a);
+        System.out.println(b);
+        b = b+1;
+        System.out.println(b);
+    }
+
+}
