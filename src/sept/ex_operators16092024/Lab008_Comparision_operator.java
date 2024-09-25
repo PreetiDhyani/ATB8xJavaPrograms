8bae93a29126f52c5b59719c83b0ff7991a4e0fa
Preeti <preetidhyani21@gmail.com>
2024-09-25 09:56:38 +0530

    All the files

---
+++ src/sept/ex_operators16092024/Lab008_Comparision_operator.java
@@ -0,0 +1,10 @@
+package sept.ex_operators16092024;
+
+public class Lab008_Comparision_operator {
+    public static void main(String[] args) {
+        int a = 10;
+        boolean b = (10==11);
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
