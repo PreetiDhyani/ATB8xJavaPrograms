ca6d47a0b1a6e6047fa328000939f9ee48fcf87b
Preeti <preetidhyani21@gmail.com>
2024-09-25 10:25:09 +0530

    All the files

---
+++ src/sept/ex_typecasting18092024/Lab001.java
@@ -0,0 +1,17 @@
+package sept.ex_typecasting18092024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 10;
+        int c = a+b;
+        System.out.println(c);
+
+        char c1 = 'A';
+        char c2 = 'B';
+        int H = c1 + c2;
+        System.out.println(H);
+
+
+    }
+}
