e95429973431b298ac6e46c81f7115d315919f91
Preeti <preetidhyani21@gmail.com>
2024-09-27 05:25:13 +0530

    All the files

---
+++ src/sept/ex_t20092024_Ternaryoperator/Lab001.java
@@ -0,0 +1,8 @@
+package sept.ex_t20092024_Ternaryoperator;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        int a1 = (30 > 40) ? 10 : 20;
+        System.out.println(a1);
+    }
+}
---
+++ src/sept/ex_t20092024_Ternaryoperator/Lab002.java
@@ -0,0 +1,11 @@
+package sept.ex_t20092024_Ternaryoperator;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        int Preeti = 40;
+        int Sushant = 30;
+        String result = Preeti>Sushant ? "Preeti Won" : "Sushant Won"; // Condition ? expression_if_true : expression_if_false;
+        System.out.println(result);
+
+    }
+}
